3d83467f768e2c4401bfd1fd940f620554276649
XGroup <138381865+webdevusers@users.noreply.github.com>
2023-12-03 23:22:24 +0100

    Update router.js

--- Product/router/router.js
+++ Product/router/router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/create', controller.create)
 router.post('/get', controller.getItems)
+router.post('/update', controller.update)
 
 module.exports = router;
