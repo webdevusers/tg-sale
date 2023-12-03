6bb09ca08302c90fdd46866a777f87ee523cbba1
XGroup <138381865+webdevusers@users.noreply.github.com>
2023-12-03 23:15:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 const authRouter = require('./Auth/router/router')
 const itemRouter = require('./Product/router/router')
